c9f60e27c0d817faccecdfcdae9f1f69bdaf3134
axsk <axsk@users.noreply.github.com>
2022-05-04 09:59:31 +0200

    check all subcalls

--- src/DeepMemo.jl
+++ src/DeepMemo.jl
@@ -2,54 +2,81 @@ using Cassette
 
 Cassette.@context TimeCtx
 
-mutable struct Call2
-    method
-    source
+mutable struct MethodCall
+    args
     time
-    in
-    out
+    result
+    reflection
     history
 end
 
-function Cassette.overdub(ctx::TimeCtx, args...)
-    subtrace = Any[]
+CACHE = Dict()
+
+@generated function Cassette.overdub(ctx::TimeCtx, args...)
+    reflection = Cassette.reflect(args)
+    if isa(reflection, Cassette.Reflection)     
+        if reflection.method.module == Main
+            return :(myoverdub(ctx, args...))
+        end
+    end
+    return :(Cassette.fallback(ctx, args...))
+end
+
+function myoverdub(ctx::TimeCtx, args...)
+
+    if haskey(CACHE, args) && true
+        call = CACHE[args]
+        if checkintegrity(call.reflection)
+            println("found cache $args")
+            return call.result
+        else
+            println("cache invalidated $args")
+            delete!(CACHE, args)
+        end
+    end
     
     if Cassette.canrecurse(ctx, args...)
-
-        newctx = Cassette.similarcontext(ctx, metadata = subtrace)
-
-        time = @elapsed res = Cassette.recurse(newctx, args...)
-        reflection = Cassette.reflect(typeof.(args))
-        
-        if reflection === nothing  ## this happened e.g. in x^2 with args = (Val{2},)
-            hist = []
-            @show args
-        else
-            hist = [reflection]
-        end
-        
-        for c in newctx.metadata
-            #push!(hist, c.source)
-            #@show c
-            #@show c.history
-            append!(hist, c.history)
-        end
-       
-        #push!(hist, reflection)
-
-
-        call = Call2(args[1], reflection, time, args[2:end], res, hist)
-
-        push!(ctx.metadata, call)
-        return res
+        return MethodCall(ctx, args)
     else
         return Cassette.fallback(ctx, args...)
     end
 end
 
-trace = Any[]
-x, y, z = 1,2,3
-f(x, y, z) = test1(x)
+
+function MethodCall(ctx, args)
+    newctx = Cassette.similarcontext(ctx, metadata = Meta(args))
+
+    time = @elapsed result = Cassette.recurse(newctx, args...)
+    #@show args, newctx.metadata.deps
+    #@show args, ctx.metadata, newctx.metadata
+    deps = union!(ctx.metadata.deps, newctx.metadata.deps) # add (sub-)calls to parent ctx
+
+    call = MethodCall(args, time, result, nothing, deps)
+
+    ctx.metadata.cached[1] += newctx.metadata.cached[1]
+
+    if time - ctx.metadata.cached[1] > 0.1
+        println("adding to cache ", call.args[1])
+        @show ctx.metadata.cached[1] = time
+        call.reflection = Set(Cassette.reflect.(deps))
+        push!(CACHE, (call.args)=>call)
+    end
+
+    return result
+end
+
+Meta() = (; deps=Set{Tuple}(), cached=[0.])
+Meta(args) = (; deps=Set{Tuple}((typeof.(args), )), cached=[0.])
+
+f(x) = test1(x)
+function test()
+    #global CACHE = Dict()
+    trace = Meta()
+    res = Cassette.overdub(TimeCtx(metadata = Meta()), f, collect(1:3))
+    res = Cassette.overdub(TimeCtx(metadata = Meta()), f, 4)
+    res = Cassette.overdub(TimeCtx(metadata = Meta()), f, 4)
+    res, trace
+end
 
 function test1(x)
     test2(x) + test22(x)
@@ -57,15 +84,13 @@ end
 
 function test2(x)
     #println("ok")
-    2*x + 1
+    2*x .+ 2
 end
 
 function test22(x)
-    return x^2
+    sleep(0.1)
+    return x.^2
 end
-
-Cassette.overdub(TimeCtx(metadata = trace), f, x, y, z)
-trace
 
 function checkintegrity(history)
     for ref in history
